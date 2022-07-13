e8d4c8c35fbd0af5d876a00ff2c59432315a480b
Felipe Nunes <felipedevnunes@gmail.com>
2022-07-13 17:22:09 -0300

    feat: :sparkles: User Model

---
+++ src/database/models/user.js
@@ -0,0 +1,39 @@
+const {DataTypes} = require('sequelize');
+
+const attr = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  displayName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull:false,
+    type:DataTypes.STRING,
+  },
+  password: {
+    allowNull:false,
+    type:DataTypes.STRING,
+  },
+  image: {
+    allowNull:false,
+    type:DataTypes.STRING,
+  }
+}
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize) => {
+  const model = sequelize.define('User', attr, {
+    tableName:'Users',
+  });
+  return model;
+};
