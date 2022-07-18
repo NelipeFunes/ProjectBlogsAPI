b229c92e6132a6cb07d15a1644a4033a3dd808d0
Felipe Nunes <felipedevnunes@gmail.com>
2022-07-18 18:42:15 -0300

    feat: Req 07

---
+++ src/database/models/category.js
@@ -0,0 +1,29 @@
+const {DataTypes} = require('sequelize');
+
+const attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull:false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+};
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize) => {
+  const model = sequelize.define('Category', attributes, {
+    tableName: 'Category',
+    timestamps: false
+  });
+  return model;
+}
